fb4c2579306c0e0e7173124f01984f43c0e7d73c
Mengyue Zhang <mengyue.z@outlook.com>
2020-11-22 16:03:57 -0500

    upload finished code

---
+++ Develop/script.js
@@ -0,0 +1,61 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Create password options as arrays
+var numericChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var alpha = "abcdefghijklmnopqrstuvwxyz"
+var lowerCaseChar = alpha.split("");
+var upperCaseAlpha = alpha.toUpperCase();
+var upperCaseChar = upperCaseAlpha.split("");
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", ".", "|", "~", "?"];
+
+// prompts to determin password criterias 
+function generatePassword() {
+
+  var passwordLength = prompt("Please enter the length of your password. (Password must be between 8 to 128 characters.)");
+  passwordLength = parseInt(passwordLength);
+  var passwordOptions = [];
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters. Please enter again.");
+  } else {
+    var wantNumber = confirm("Do you want numbers in your password? please click ok to select.");
+    if (wantNumber) {
+      passwordOptions = passwordOptions.concat(numericChar);
+    }
+    var wantLowerCase = confirm("Do you want lowercase characters in your password? please click ok to select.");
+    if (wantLowerCase) {
+      passwordOptions = passwordOptions.concat(lowerCaseChar);
+    }
+    var wantUpperCase = confirm("Do you want uppercase characters in your password? please click ok to select.");
+    if (wantUpperCase) {
+      passwordOptions = passwordOptions.concat(upperCaseChar);
+    }
+    var wantSpecialChar = confirm("Do you want special characters in your password? please click ok to select.");
+    if (wantSpecialChar) {
+      passwordOptions = passwordOptions.concat(specialChar)
+    }
+    console.log(passwordOptions);
+    if (passwordOptions.length === 0) {
+      alert("Please select at least one character type for you password");
+    } else {
+      var randomPassword = [];
+      for (var i = 0; i < passwordLength; i++) {
+        randomIndex = Math.floor(Math.random() * passwordOptions.length);
+        randomPassword += passwordOptions[randomIndex];
+        console.log(randomPassword);
+      }
+    }
+  }
+  return (randomPassword);
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# week3-homework
+# week3-homework: Password Generator
+
+# Description
+
+This is an application that people can use to generate a random password based on criteria they have selected.
+Firstly people will decide the length of the password they like (Must be between 8 and 128 characters).
+Then they will choose if they want numbers, uppercase and lowercase letters and special characters in their password (at least one character type need to be selected).
+After that the randomly generated password will be displayed on the page.
+
+# Screenshots
+
+The following image shows this Password Generator Application's appearance and functionality:
+
+![password generator homework demo](./screenshots/Password-Generator.jpg)
+
+# Links
+
+* The URLs of the deployed website: https://mengyue-z.github.io/week3-homework
+
+* The URLs of the GitHub repositories: https://github.com/mengyue-z/week3-homework
+
